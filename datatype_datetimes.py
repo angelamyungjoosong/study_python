a475b09d9dbf4883ccf38926905190c8e8183bb9
angelamyungjoosong <angelaforever122@gmail.com>
2023-08-04 18:22:11 +0900

    8.4

---
+++ datatype_datetimes.py
@@ -0,0 +1,17 @@
+import datetime 
+
+# 오늘 날짜 (print today)
+today = datetime.datetime.now()
+# pakcage.class.method
+pass
+
+# today
+# datetime.datetime(2023, 8, 4, 18, 14, 7, 800525)
+# 변수에 각각 담겨있다(셋팅된 값이 int형으로 들어있다)
+
+
+# type(today)
+# <class 'datetime.datetime'>
+
+yesterday = datetime.datetime(2023, 8, 3)
+# 넣은 값을 그대로 셋팅하고 있다 
